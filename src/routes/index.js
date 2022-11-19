6ae396aec35a0c0ebdc7d931f0b3970eb85aa17d
Dinh Nhu Tan <nhutan2001@gmail.com>
2022-11-19 14:41:33 +0700

    feedback api implementation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+<<<<<<< HEAD
 const HomeController = require('../app/controllers/homeController');
 const UserController = require('../app/controllers/userController');
 const DoctorController = require('../app/controllers/doctorController');
@@ -26,5 +27,16 @@ router.post('/user/:id/update', UserController.updateUser);
 router.get('/doctor/search', DoctorController.getDoctorsByName);
 router.get('/doctor/search/major', DoctorController.getDoctorsByMajor);
 router.post('/doctor/profile', DoctorController.updateProfile);
+=======
+const HomeController = require('../app/controllers/homeController')
+const FeedbackController = require('../app/controllers/feedBackcontroller.js')
+
+router.get('/', HomeController.getUsers);
+router.post('/resgister', HomeController.register);
+router.post('/login', HomeController.login);
+router.post('/feedback', FeedbackController.createFeedback);
+router.get('/feedback', FeedbackController.getAllFeedback);
+router.get('/feedback/:id', FeedbackController.getFeedback);
+>>>>>>> feedback api implementation
 
 module.exports = router;
