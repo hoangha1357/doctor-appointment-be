e9b37baff23fc5b43c1e30216c1282f06dbf0c6a
hoangha1357 <76683867+hoangha1357@users.noreply.github.com>
2022-11-24 22:11:12 +0700

    merge dev

--- src/app/controllers/doctorController.js
+++ src/app/controllers/doctorController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const DoctorProfile = require('../models/DoctorProfile');
 class DoctorController {
+    
     updateProfile = async (req, res, next) => {
         const { major, certificate } = req.body;
     
